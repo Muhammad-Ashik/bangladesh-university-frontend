b571ccaa51d77fd62fe6f27eb706415b5ca78070
muhammad-ashik <ashik.chowdhury79@gmail.com>
2024-11-18 00:40:13 +0600

    Refactor Carousel and HeroSection: add itemClassName prop, remove unused CSS files, and update layout styles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,10 +3,9 @@
 import DesktopNavBar from './desktop/DesktopNavbar'
 import MobileDrawer from './mobile/MobileDrawer'
 import MobileNavBar from './mobile/MobileNavBar'
-import styles from './Navbar.module.css'
 
 const NavBar = () => (
-  <nav className={styles.navbar}>
+  <nav>
     <div className='xl:container mx-auto px-4 py-3'>
       <DesktopNavBar />
       <MobileNavBar />
