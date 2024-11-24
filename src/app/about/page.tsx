7f711903a20f26555f5dfbf030e33f91b15b5203
itzJunaedRayhan <webgenesis99@gmail.com>
2024-11-24 10:43:36 +0600

    feature:Navigation-Header

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import NavigationHeader from '@/components/common/NavigationHeader'
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <NavigationHeader />
+    </div>
+  )
+}
